80c360e0de9eb95ca366278aa348be255e0814a4
shankesleroux8988 <shankesleroux8988@gmail.com>
2018-08-17 01:32:13 +0800

    Fix bubbleShooter

--- BubbleShooter/Assets/Scripts/Game/Script/GamePlay/Bubble/BubbleShooter.cs
+++ BubbleShooter/Assets/Scripts/Game/Script/GamePlay/Bubble/BubbleShooter.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class BubbleShooter : MonoBehaviour
 {
     private const float ROTATE_SPEED = 90f;
-    private const float LEFT_MAX_ANGLE = 85f;
-    private const float RIGHT_MAX_ANGLE = 275f;
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.back * ROTATE_SPEED * Time.deltaTime*-1);
+            transform.Rotate(Vector3.back * ROTATE_SPEED * Time.deltaTime * -1);
+            Debug.Log(currentRotation.z);
         }
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back * ROTATE_SPEED * Time.deltaTime);
+            Debug.Log(currentRotation.z);
+        }
 
+
+        if (currentRotation.z > 65.0f && currentRotation.z < 180.0f)
+        {
+            currentRotation.z = 65.0f;
+            transform.localRotation = Quaternion.Euler(currentRotation);
+        }
+
+        else if(currentRotation.z<295.0 && currentRotation.z >180.0f)
+        {
+            currentRotation.z = 295.0f;
+            transform.localRotation = Quaternion.Euler(currentRotation);
         }
     }
 }
